22c73af60ba9119961ee180f2618e81bb216f5d3
Roger Winter <roger.winter@circleci.com>
2026-04-15 13:36:01 -0700

    Add CSV and JSON export/import endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import taskRoutes from './routes/tasks';
+import exportRoutes from './routes/export';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.get('/health', (_req, res) => {
 });
 
 app.use('/tasks', taskRoutes);
+app.use('/export', exportRoutes);
 
 export default app;
